a87c4e7c326e3558521bc43ecca1b491cd0b1209
vladimirGOU <insta000111@ukr.net>
2023-01-05 14:41:32 +0100

    joi validation

---
+++ joi_validation/joi_validation.js
@@ -0,0 +1,17 @@
+const express = reqire("express");
+const Joi = reqire("joi");
+const app = express();
+
+app.use(express.json());
+
+const signupSchema = Joi.object({
+    
+})
+
+app.post("/sighup", (req, res) => {
+    res.send("Successfully signet up!")
+})
+
+app.listen(4000, () => {
+    console.log("Server started on port 4000");
+})
