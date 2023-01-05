0e0d5e86ffa6a5dfd8d8c9b988054d1bdb84c9e2
vladimirGOU <insta000111@ukr.net>
2023-01-05 12:24:25 +0100

    Crocoder

---
+++ Crocoder/index.js
@@ -0,0 +1,11 @@
+
+const input = [ 1, 2, 3, 4, 5 ];
+// const map1 = input.map(x => x * x);
+// console.log('Solution 1 ', map1);
+
+let map2 = input.map( num => Math.pow(num, 2));
+
+console.log('Solution 2 ', map2);
+
+Math.pow
+Math.pow()
