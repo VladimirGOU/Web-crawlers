68599ded2fd5f499cabe4d56dcea6df0b8737ac3
vladimirGOU <insta000111@ukr.net>
2023-01-30 09:08:05 +0100

    do while loop

--- js-basics/js-basics/1_video_examples/Section_4_control_flow/for.js
+++ js-basics/js-basics/1_video_examples/Section_4_control_flow/for.js
@@ -30,3 +30,116 @@ for (let iDivM = 5; iDivM >=1; iDivM--){
     console.log('Remaine of Division Backwars', iDivM);
 }
 
+// DO WHILE LOOP do-while loops are always executed at least once. Even if this condition evaluates to false
+
+for (let start = 0; start<=5 ; start++) {
+    if ( start % 2 !== 0) console.log('Do while', start)
+}
+
+let whileLoop = 9
+while( whileLoop <= 5) {
+    if ( whileLoop % 2 !==0) console.log('While loop', whileLoop);
+    whileLoop++;
+}
+
+//INFINITE LOOP
+
+
+//FOR LOOPS
+//WHILE LOOPS
+//DO WHILE LOOPS
+
+function maxOfTwoNUmbers(a, b){
+   if (a > b) {
+    return a
+   } else return b;}
+let maxOfTwo = maxOfTwoNUmbers(1,2)
+console.log(maxOfTwo)
+
+
+const lar = largrer(10, 12)
+function largrer (number1, number2) {
+//    let sum = number1 > number2 ? number1 : number2;
+//    return sum;
+      return number1 > number2 ? number1 : number2
+};
+console.log('largest =',lar)
+
+//Types of function exrpession
+//Function Declaration
+function inc(a) {
+return a + 1;
+}
+//Function expression
+const suma = function(a, b) {
+    return a + b
+};
+//Lambda expression or Array function
+const sum = (a, b) => (a + b);
+const maxOf = (a, b) => (a > b ? a : b); 
+console.log(maxOf(18,17));
+
+const avg = (x, y) => {
+    let s = suma(x, y);
+    return s/2;
+}
+console.log(avg(10, 3))
+
+const cicties = ['Odessa', 'Kyiv', 'Malga', 'Madrid', 'London', 'New York'];
+const f = s => s.length;
+function f2(s) {
+    return 'index' + s.length
+}
+console.log(cicties)
+console.log( cicties.map(f2))
+
+function f1 () {
+    const cicties = ['Odessa', 'Kyiv'];
+    const f = s => s.toUpperCase();
+    console.dir({cicties})
+    console.dir(cicties.map(f))
+
+    {
+        const f = s => s.toLowerCase();
+        console.dir({cicties});
+        console.dir(cicties.map(f));
+    }
+
+    {
+        const cicties = ['Malga', 'Madrid', 'London'];
+        console.dir({cicties});
+        console.dir(cicties.map(f));
+    }
+}
+
+console.log(isLandscape(800,900))
+function isLandscape(width, height) {
+    // return (width > height) ? true: false;
+    return (width > height)
+
+}
+
+const output = fizzBuzz(false)
+console.log(output)
+
+function fizzBuzz(input){
+    if ( typeof input !== 'number') {
+        return NaN
+    } 
+
+    if ( input %3 === 0 && input %5 ===0 ) {
+        return "FizzBuzz"
+    }
+
+    if ( input %3 === 0) {
+        return 'Fizz'
+    }
+
+    if (input %5 === 0 ) {
+        return 'Buzz'
+    }
+
+    return input
+}
+
+
