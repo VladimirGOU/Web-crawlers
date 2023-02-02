d5a5e5a7df37955a6703953e9352fbb8375a3b57
vladimirGOU <insta000111@ukr.net>
2023-02-02 09:42:30 +0100

    Arrsays

---
+++ js-basics/js-basics/1_video_examples/Section_5_Objects/basics.js
@@ -0,0 +1,33 @@
+// let radius = 1;
+// let x = 1;
+// let y = 1;
+
+// const circle = {
+//     radius: 1,
+//     location:{
+//         x:1,
+//         y:1,
+//     },
+//     isVisible: true,
+//     draw: function(){
+//         lonsole.log('draw');
+//     }
+// };
+
+// function draw() {}
+// function move() {}
+
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+   isVisible: true, 
+   draw: function(){
+    console.log('draw')
+   }
+};
+
+circle.draw(); //Method
